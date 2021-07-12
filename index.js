475e5d1a89d122bb8e6f605cb5992706f21ed434
Nicole Gathany <nicole.gathany@gmail.com>
2021-07-12 19:44:51 +0000

    just removed comments

--- index.js
+++ index.js
@@ -3,14 +3,7 @@
 //For example, repeats([4,5,7,5,4,8]) = 15 because only the numbers 7 and 8 occur once, and their sum is 15. Every other number occurs twice.
 
 const repeats = arr => {
-  //given an array
-  //some numbers are there twice, some are there 1
-  //return the sum of numbers that have a freq of 1
-  //.indexOf() and .lastIndexOf()
-  //for loop with .indexOf() and .lastIndex()
-  // for(let i=0; i<arr.length; i++){
-  //   console.log(arr[i], " is at ", arr.indexOf(arr[i]), " and ", arr.lastIndexOf(arr[i]))
-  // }
+
   console.log(arr.filter(el => arr.indexOf(el)===arr.lastIndexOf(el)))
  return arr.filter(el => arr.indexOf(el)===arr.lastIndexOf(el)).reduce((acc, curr)=> acc + curr, 0)
 
